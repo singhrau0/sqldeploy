cabcb66f433a6f270ef7bf1b7e6ec89d6580f548
Aman <aman@katalisia.com>
2023-06-01 14:11:30 +0530

    hey

---
+++ main.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/show-customers')
+def customer_show():
+    conn = sqlite3.connect('mydatabase.db')
+    cn = conn.cursor()
+    cn.execute("select * from customer")
+    data = []
+    for i in cn.fetchall():
+        customer = {}
+        customer['customer_id']= i[0]
+        customer['customer_name'] = i[1]
+        customer['customer_addr'] = i[2]
+        customer['customer_email'] = i[3]
+        data.append(customer)
+    conn.close()
+    return render_template('showcustomers.html',data = data)
+
+if __name__ == '__main__':
+    app.run()
